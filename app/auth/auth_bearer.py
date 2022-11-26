b381d8ca15c43defb85a3daceff66ea341a0eae4
salaheddine <salaheddine@leavy.co>
2022-11-26 13:52:51 +0100

    Fix import issue

--- app/auth/auth_bearer.py
+++ app/auth/auth_bearer.py
@@ -1,7 +1,7 @@
 from fastapi import Request, HTTPException
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 
-from app.auth.auth_bearer import decodeJWT
+from app.auth.auth_handler import decodeJWT
 
 
 class JWTBearer(HTTPBearer):
