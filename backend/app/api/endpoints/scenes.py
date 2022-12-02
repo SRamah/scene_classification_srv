df23ec05b21f499ea907bedc91ccf22cdb8c2a3e
salaheddine <salaheddine@leavy.co>
2022-12-02 13:41:56 +0100

    scene classification v0.2: Add Front-end

---
+++ backend/app/api/endpoints/scenes.py
@@ -0,0 +1,23 @@
+from fastapi import APIRouter, Depends, HTTPException
+from typing import List, Dict
+from app.auth.auth_bearer import JWTBearer
+from plugins.scenes.classifier import SceneClassification
+from app import schemas
+
+SC = SceneClassification()
+
+router = APIRouter()
+
+@router.get("/", response_model=Dict)
+def root():
+    return {"message": "Welcome to Scene Classification Service."}
+
+@router.put("/url/label/", response_model=List[schemas.SceneClassifierOutput], dependencies=[Depends(JWTBearer())])
+async def scene_label_from_urls(images: List[str]):
+    results = SC.img_urls_classification(images)
+    return results
+
+@router.put("/obj/label/", response_model=List[schemas.SceneClassifierOutput], dependencies=[Depends(JWTBearer())])
+async def scene_label_from_img(image: object):
+    results = SC.img_obj_classification(image)
+    return results
